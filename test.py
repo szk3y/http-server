f4163124a4b074d325b288a26c9947b0cefaa9cb
szk3y <d4tt423@gmail.com>
2018-06-09 03:23:07 +0900

    Update test.py to use timeout in requests instead of alarm

--- test.py
+++ test.py
@@ -1,25 +1,13 @@
 #!/usr/bin/env python3
 
 import requests
-import signal
 from subprocess import Popen
-
-class Timeout(Exception):
-    pass
-
-def notice_timeout(*args):
-    raise Timeout
 
 target = './http-server'
 
-signal.signal(signal.SIGALRM, notice_timeout)
 p = Popen([target])
 try:
-# set alarm to automatically terminate the GET request
-    signal.alarm(1)
-    requests.get('http://localhost:8080/main.cc')
-except Timeout:
+    requests.get('http://localhost:8080/main.cc', timeout=1)
+except requests.exceptions.Timeout:
     print('Timeout!')
-# turn off alarm
-signal.alarm(0)
 p.terminate()
