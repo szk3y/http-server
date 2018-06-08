646833b6afcede39db72db2e903f8104d1fa81bc
szk3y <d4tt423@gmail.com>
2018-06-09 03:18:44 +0900

    Update test

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 
 import requests
 import signal
+from subprocess import Popen
 
 class Timeout(Exception):
     pass
@@ -9,12 +10,16 @@ class Timeout(Exception):
 def notice_timeout(*args):
     raise Timeout
 
+target = './http-server'
+
 signal.signal(signal.SIGALRM, notice_timeout)
+p = Popen([target])
 try:
 # set alarm to automatically terminate the GET request
-    signal.alarm(2)
+    signal.alarm(1)
     requests.get('http://localhost:8080/main.cc')
 except Timeout:
     print('Timeout!')
 # turn off alarm
 signal.alarm(0)
+p.terminate()
